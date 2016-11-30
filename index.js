05cd9ad1961781b850325ff2be2b5a8254b2d6c5
Larry <larry.lo1025@gmail.com>
2016-11-30 13:29:44 +0800

    enhance filter and autoreply msg

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var options = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer [Authorization Token]'
+    'Authorization': 'Bearer [LineAuthorization]'
   }
 }
 app.set('port', (process.env.PORT || 5000));
@@ -26,15 +26,20 @@ app.get('/', function(req, res) {
 
 app.post('/', jsonParser, function(req, res) {
   let event = req.body.events[0];
+  let type = event.type;
+  let msgType = event.message.type;
   let msg = event.message.text;
   let rplyToken = event.replyToken;
 
-  let rplyVal = '';
-  try {
-    rplyVal = parseInput(rplyToken, msg); 
-  } 
-  catch(e) {
-    rplyVal = '格式錯惹，正確格式如： 1d3, 5d200';
+  let rplyVal = null;
+  console.log(msg);
+  if (type == 'message' && msgType == 'text') {
+    try {
+      rplyVal = parseInput(rplyToken, msg); 
+    } 
+    catch(e) {
+      rplyVal = randomReply();
+    }
   }
 
   if (rplyVal) {
@@ -92,7 +97,7 @@ function parseInput(rplyToken, inputStr) {
   }
 
   let commandArr = mainMsg[1].split(comSplitor);
-  if (commandArr.length != 2 || _isNaN(commandArr[0]) || _isNaN(commandArr[1])) return '格式錯惹，正確格式如： 1d3, 5d200';
+  if (commandArr.length != 2 || _isNaN(commandArr[0]) || _isNaN(commandArr[1])) return randomReply();
   let countOfNum = commandArr[0];
   let randomRange = commandArr[1];
   
@@ -114,4 +119,9 @@ function parseInput(rplyToken, inputStr) {
 
 function random(min, max) {
   return Math.floor((Math.random() * max) + min);
+}
+
+function randomReply() {
+  let rplyArr = ['幹你娘不要亂打好嗎？懂？', '你媽知道你在這裡發廢文嗎？', '啊啊啊啊～我要被玩壞惹', '幫QQ', '我看不到明天的陽光', '在非洲，每六十秒，就有一分鐘過去。認同請分享', '只要每天省下買一杯奶茶的錢，十天後就能買十杯奶茶', '當你的左臉被人打，那你的左臉就會痛', '誰能想的到，這名16歲少女，在四年前，只是一名12歲少女', '當蝴蝶在南半球拍了兩下翅膀，牠就會稍微飛高一點點', '台灣競爭力低落，在美國就連小學生都會說流利的英語', '每呼吸60秒，就減少一分鐘的壽命', '據統計，未婚生子的人數中有高機率為女性'];
+  return rplyArr[Math.floor((Math.random() * (rplyArr.length - 1)) + 0)];
 }
