c40c65990d343ad0599f0137f006b099e218df04
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-01-01 02:32:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -159,12 +159,14 @@ function ccb(chack,text){
           let temp = Dice(100);
           if (text == null ) {
             if (temp == 100) return 'ccb<=' + chack  + ' ' + temp + ' → 啊！大失敗！';
+	    if (temp == 1) return 'ccb<=' + chack  + ' ' + temp + ' → 太棒了！大成功！';
             if (temp <= chack) return 'ccb<=' + chack + ' '  + temp + ' → 成功';
             else return 'ccb<=' + chack  + ' ' + temp + ' → 失敗' ;
           }
           else
     {
             if (temp == 100) return 'ccb<=' + chack + ' ' + temp + ' → 啊！大失敗！；' + text;
+	    if (temp == 1) return 'ccb<=' + chack + ' ' + temp + ' → 太棒了！大成功！；' + text;
             if (temp <= chack) return 'ccb<=' + chack +  ' ' + temp + ' → 成功；' + text;
             else return 'ccb<=' + chack  + ' ' +  temp + ' → 失敗；' + text;
     }
@@ -548,7 +550,8 @@ returnStr  += '/' + varcou.reduce(function(previousValue,currentValue){
 		  	'\我的製作人，施彥任內心有點脆弱，拜托沒事不要傷他的心喔。',
 		  	'\比起一直找我聊天，不如試著找其他事做吧。',
 		  	'\我最喜歡的角色嗎...？應該是露吧。不論何時都很可愛',
-		  	'\稍微...讓我休息一下吧(攤'];
+		  	'\稍微...讓我休息一下吧(攤',
+			'\希望我成為你的同伴？看看夥伴商店吧！'];
         	return rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
         }
 ////////////////
@@ -993,42 +996,28 @@ function tarotCardReply(count) {
 
 		function Help() {
 			return randomReply() + '\n' + '\
-【擲骰BOT】v1.26 \
-\n 例如輸入2d6+1　攻撃！\
-\n 會輸出）2d6+1：攻撃  9[6+3]+1 = 10\
+【梅里歐斯的冒險者專用BOT】v1.00 \
+\n --傷害骰(a XdY+b)--\
+\n 如果輸入2d6+1　攻撃！\
+\n 就會輸出）2d6+1：攻撃  9[6+3]+1 = 10\
 \n 如上面一樣,在骰子數字後方隔空白位打字,可以進行發言。\
 \n 以下還有其他例子\
 \n 5 3D6 	：分別骰出5次3d6\
-\n D66 D66s ：骰出D66 s小者固定在前\
 \n 5B10：不加總的擲骰 會進行小至大排序 \
 \n 5B10 9：如上,另外計算其中有多少粒大過9 \
 \n 5U10 8：進行5D10 每骰出一粒8會有一粒獎勵骰 \
 \n 5U10 8 9：如上,另外計算其中有多少粒大過9 \
-\n Choice：啓動語choice/隨機/選項/選1\
-\n (問題)(啓動語)(問題)  (選項1) (選項2) \
-\n 例子 隨機收到聖誕禮物數 1 2 3 >4  \
-\n  \
-\n 隨機排序：啓動語　排序\
-\n (問題)(啓動語)(問題)  (選項1) (選項2)(選項3) \
-\n 例子 交換禮物排序 A君 C君 F君 G君\
-\n \
-\n ・COC六版判定　CCb （目標値）：做出成功或失敗的判定\
+\n--機率骰(ccb)--\
+\n CCb （目標値）：做出成功或失敗的判定\
 \n例）CCb 30　CCb 80\
-\n ・COC七版判定　CCx（目標値）\
-\n　x：獎勵骰/懲罰骰 (2～n2)。沒有的話可以省略。\
-\n  \
-\n ・cc六版創角\
-\n ・cc七版創角 （年齡）\
-\n 	依戀  NM (問題) \
-\n  例子 NM NM 我的依戀\
-\n  \
-\n・WOD 黑暗世界擲骰\
-\n (骰數)WOD/Wd(加骰)(+成功數) (問題)\
-\n  例子 2wod 3wd8 15wd9+2\
-\n  \
+\n--Bot重要功能--\
+\n輸入 招募，就能確認當前的招募目錄\
+\n輸入 寶箱/開寶箱，就能確認你剛獲得的寶箱內容喔\
+\n--Bot娛樂功能--\
 \n・占卜運氣功能 字句中包括運氣即可\
-\n・塔羅牌占卜 塔羅/大十字塔羅/每日塔羅牌\
-\n  時間tarot 等關键字可啓動\
-\n  死亡FLAG：啓動語 立Flag/死亡flag\
+\n・塔羅牌占卜 塔羅/大十字塔羅/每日塔羅牌/時間tarot 等關键字可啓動\
+\n  死亡FLAG：句子裡出現 Flag/flag 就能讓你輕鬆插旗\
+\n 輸入 猜拳，就能和空音猜拳\
+\n 如果呼叫空音的名子...好像會有事情發生？\
 ';		
 		}
