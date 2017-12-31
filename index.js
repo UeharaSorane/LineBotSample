6b04f94b3d2602d8b8ff7c8c3dd74cb8cae5afa0
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2017-12-31 22:06:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -88,25 +88,28 @@ function replyMsgToLine(rplyToken, rplyVal) {
 function parseInput(rplyToken, inputStr) {
           
 	//	console.log('InputStr: ' + inputStr);
-		_isNaN = function(obj) {
-			return isNaN(parseInt(obj));
-        }                   
-        let msgSplitor = (/\S+/ig);	
-		let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
-		let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
-                       
-        //鴨霸獸指令開始於此
-        if (trigger.match(/空音/) != null) return randomReply() ;
-	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return BoxReply() ;        
-        if (trigger.match(/運氣|運勢/) != null) return randomLuck(mainMsg) ; //占卜運氣        
-        
-		//FLAG指令開始於此
-        if (trigger.match(/flag/) != null) return BStyleFlagSCRIPTS() ;        
-       
+	_isNaN = function(obj) {
+		return isNaN(parseInt(obj));
+        }
 		
-
-		//依戀
-		if (trigger.match(/(^nm$)/) != null)	 return nechronica_mirenn(mainMsg[1]);
+        let msgSplitor = (/\S+/ig);	
+	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
+	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
+	
+	////////////////////////////情報相關
+	
+	
+	////////////////////////////戰鬥相關
+	
+	
+	////////////////////////////服務相關
+	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return BoxReply() ;//寶箱狩獵指令
+	
+	////////////////////////////娛樂相關
+        if (trigger.match(/空音/) != null) return randomReply() ;//空音閒談指令
+	if (trigger.match(/運氣|運勢/) != null) return randomLuck(mainMsg) ; //占卜運氣        
+        if (trigger.match(/flag/) != null) return BStyleFlagSCRIPTS() ;//插旗用指令
+       
 			
 		if (trigger.match(/(^cc7版創角$|^cc七版創角$)/) != null && mainMsg[1] != NaN )	 return build7char(mainMsg[1]);
 	
@@ -628,42 +631,6 @@ function BuildRollDice(inputStr){
   return finalStr;
 }
             
-
-////////////////////////////////////////
-//////////////// nechronica (NM依戀)
-////////////////////////////////////////
-
-function nechronica_mirenn(text) {
-	let returnStr = '';
-	var dicenew = 0;
-	dicenew = Dice(10)-1;
-
-	// 產生格式
-	if (text != null)
-		returnStr = text + ': \n' + '依戀 (' + (dicenew+1) + '[' + (dicenew+1) + ']) → ' + nechronica_mirenn_table(dicenew);
-	else
-		returnStr = '依戀 (' + (dicenew+1) + '[' + (dicenew+1) + ']) → ' + nechronica_mirenn_table(dicenew);
-
-	return returnStr;
-}
-
-/* 這邊預留 mode 以便未來可以加入其他依戀 */
-function nechronica_mirenn_table(mode) {
-	if (mode == 0) returnStr = '【嫌惡】\n[發狂：敵對認識] 戰鬥中，沒有命中敵方的攻擊，全部都會擊中嫌惡的對象。(如果有在射程內的話)';
-	if (mode == 1) returnStr = '【獨占】\n[發狂：獨占衝動] 戰鬥開始與戰鬥結束，各別選擇損傷1個對象的部件。';
-	if (mode == 2) returnStr = '【依存】\n[發狂：幼兒退行] 妳的最大行動值減少2。';
-	if (mode == 3) returnStr = '【執著】\n[發狂：跟蹤監視] 戰鬥開始與戰鬥結束時，對象對妳的依戀精神壓力點數各增加1點。(如果已經處在精神崩壞狀態，可以不用作此處理)';
-	if (mode == 4) returnStr = '【戀心】\n[發狂：自傷行為] 戰鬥開始與戰鬥結束時，各別選擇損傷1個自己的部件。';
-	if (mode == 5) returnStr = '【對抗】\n[發狂：過度競爭] 戰鬥開始與戰鬥結束時，各別選擇任意依戀，增加1點精神壓力點數。(如果已經處在精神崩壞狀態，可以不用作此處理)';
-	if (mode == 6) returnStr = '【友情】\n[發狂：共鳴依存] 單元結束時，對象的損傷部件比妳還要多的時候，妳的部件損傷數，要增加到與對方相同。';
-	if (mode == 7) returnStr = '【保護】\n[發狂：過度保護] 戰鬥當中，妳跟「依戀的對象」處於不同區域的時候，無法宣告「移動以外的戰鬥宣言」，此外妳沒有辦法把「自身」與「依戀對象」以外的單位當成移動對象。';
-	if (mode == 8) returnStr = '【憧憬】\n[發狂：贗作妄想] 戰鬥當中，妳跟「依戀的對象」處於同樣區域的時候，無法宣告「移動以外的戰鬥宣言」，此外妳沒有辦法把「自身」與「依戀對象」以外的單位當成移動對象。';
-	if (mode == 9) returnStr = '【信賴】\n[發狂：疑心暗鬼] 除了妳以外的所有姊妹，最大行動值減少1。';
-	return returnStr;
-}
-
-
-
 ////////////////////////////////////////
 //////////////// D66
 ////////////////////////////////////////
