800dc238f9c312a45bc3cb9d0e874a0d8c1d9790
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-01-07 21:46:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -103,49 +103,49 @@ function parseInput(rplyToken, inputStr) {
 	////////////////////////////戰鬥相關
 	//ccb指令
 	if (trigger.match(/^ccb$/)!= null && mainMsg[1]<=1000 ){
-		if (trigger == 'ccb'&& mainMsg[1]<=99) return ccb(mainMsg[1],mainMsg[2]);
+		if (trigger == 'ccb'&& mainMsg[1]<=99) return battle.ccb(mainMsg[1],mainMsg[2]);
         }
 	//xBy>A 指令開始於此
 	if (trigger.match(/^(\d+)(b)(\d+)$/i)!= null)
 	{        
-		return xBy(trigger,mainMsg[1],mainMsg[2]);
+		return battle.xBy(trigger,mainMsg[1],mainMsg[2]);
 	}
 	//xUy 指令開始於此	
 	if (trigger.match(/^(\d+)(u)(\d+)$/i)!= null && isNaN(mainMsg[1])== false)
 	{        
-		return xUy(trigger,mainMsg[1],mainMsg[2],mainMsg[3]);
+		return battle.xUy(trigger,mainMsg[1],mainMsg[2],mainMsg[3]);
 	}
 	
 	        //普通ROLL擲骰判定在此        
         if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) {
-          return nomalDiceRoller(inputStr,mainMsg[0],mainMsg[1],mainMsg[2]);
+          return battle.nomalDiceRoller(inputStr,mainMsg[0],mainMsg[1],mainMsg[2]);
         }
 	
 	////////////////////////////服務相關
-	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return BoxOpen() ;//寶箱狩獵指令
-	if (trigger.match(/^help$|^幫助$/)!= null ) return Help();//幫助頁面
-	if (trigger.match(/^招募$/) != null) return gacha(mainMsg[1],mainMsg[2]);	//角色招募指令
-	if (trigger.match(/^公告$/) != null) return GameInformation(mainMsg[1]);	//遊戲公告指令
-	if (trigger.match(/^活動$/) != null) return GameEvent(mainMsg[1]);	//遊戲活動指令
-	if (trigger.match(/^主線$/) != null) return MainStory(mainMsg[1],mainMsg[2]);	//遊戲活動指令
+	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return battle.BoxOpen() ;//寶箱狩獵指令
+	if (trigger.match(/^help$|^幫助$/)!= null ) return battle.Help();//幫助頁面
+	if (trigger.match(/^招募$/) != null) return battle.gacha(mainMsg[1],mainMsg[2]);	//角色招募指令
+	if (trigger.match(/^公告$/) != null) return battle.GameInformation(mainMsg[1]);	//遊戲公告指令
+	if (trigger.match(/^活動$/) != null) return battle.GameEvent(mainMsg[1]);	//遊戲活動指令
+	if (trigger.match(/^主線$/) != null) return battle.MainStory(mainMsg[1],mainMsg[2]);	//遊戲主線指令
 	
 	////////////////////////////娛樂相關
-        if (trigger.match(/空音/) != null) return randomReply() ;//空音閒談指令
-	if (trigger.match(/空空/) != null) return randomReplyShin() ;//空音閒談指令(裏)
-	if (trigger.match(/運氣|運勢/) != null) return randomLuck(mainMsg) ; //占卜運氣        
-        if (trigger.match(/flag/) != null) return BStyleFlagSCRIPTS() ;//插旗用指令
+        if (trigger.match(/空音/) != null) return battle.randomReply() ;//空音閒談指令
+	if (trigger.match(/空空/) != null) return battle.randomReplyShin() ;//空音閒談指令(裏)
+	if (trigger.match(/運氣|運勢/) != null) return battle.randomLuck(mainMsg) ; //占卜運氣        
+        if (trigger.match(/flag/) != null) return battle.BStyleFlagSCRIPTS() ;//插旗用指令
 	//塔羅牌
 	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
 			if (trigger.match(/每日|daily/)!= null) {
-				return NomalDrawTarot(mainMsg[1], mainMsg[2]);
+				return battle.NomalDrawTarot(mainMsg[1], mainMsg[2]);
 			}
 			if (trigger.match(/時間|time/)!= null) {
-				return MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
+				return battle.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
 			}
 			if (trigger.match(/大十字|cross/)!= null) {
-				return MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
+				return battle.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
 			}
-			return MultiDrawTarot(mainMsg[1], mainMsg[2], 3); //預設抽 79 張
+			return battle.MultiDrawTarot(mainMsg[1], mainMsg[2], 3); //預設抽 79 張
 	}
 
 
