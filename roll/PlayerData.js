743541976394a90ed918139fbe5ff87954bac750
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-14 21:08:28 +0800

    Update PlayerData.js

--- roll/PlayerData.js
+++ roll/PlayerData.js
@@ -5,10 +5,19 @@ var creds = require('./client_secret.json');
 var DB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA/edit#gid=0');
 
 function main() {
+	DB.useServiceAccountAuth(creds, function (err) {
+ 
+	  // Get all of the rows from the spreadsheet.
+	  DB.getRows(1, function (err, rows) {
+	    console.log(rows);
+	  });
+	});
+	
+	
 			
 	///確認玩家資料
       
-	rply.text =  '系統連結沒有問題';
+	rply.text =  '資料庫連結沒有問題';
 				
 	return rply;
 	
