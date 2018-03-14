0b684aefc888452a95517ba582521ce58850ba84
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-14 20:35:37 +0800

    Create Test.js

---
+++ roll/Test.js
@@ -0,0 +1,19 @@
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+function main() {
+			
+      ///確認系統沒有問題用
+      
+				rply.text =  '系統連結沒有問題';
+				
+		return rply;
+		
+		
+		///
+		
+	}
+////////////////
+
+module.exports = {
+	main
+};
