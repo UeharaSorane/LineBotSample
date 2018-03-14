0471a936a5a4bbdfbcd94a178f1c883e6ed25772
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-14 20:36:30 +0800

    Update Test.js

--- roll/Test.js
+++ roll/Test.js
@@ -2,17 +2,16 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 
 function main() {
 			
-      ///確認系統沒有問題用
+	///確認系統沒有問題用
       
-				rply.text =  '系統連結沒有問題';
+	rply.text =  '系統連結沒有問題';
 				
-		return rply;
-		
-		
-		///
-		
-	}
-////////////////
+	return rply;
+	
+	///
+
+}
+
 
 module.exports = {
 	main
