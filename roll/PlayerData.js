13097f6c63cf6df163585bab443ed7731a762a61
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-14 22:42:04 +0800

    Update PlayerData.js

--- roll/PlayerData.js
+++ roll/PlayerData.js
@@ -5,11 +5,13 @@ var creds = require('../client_secret.json');
 var DB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA');
 
 function main(UserID) {
+	 let PlayerNumber = 0;
+	
 	DB.useServiceAccountAuth(creds, function (err) {
  
 	  // Get all of the rows from the spreadsheet.
 	  DB.getRows(1, function (err, rows) {
-	    console.log(rows);
+		PlayerNumber = rows.length;
 	  });
 	});
 	
@@ -17,7 +19,8 @@ function main(UserID) {
 			
 	///確認玩家資料
       
-	rply.text =  '資料庫連結沒有問題';
+	rply.text =  '資料庫連結沒有問題\
+			\n一共有' + PlayerNumber + '筆玩家資料';
 				
 	return rply;
 	
