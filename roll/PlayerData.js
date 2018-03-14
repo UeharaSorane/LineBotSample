4335d285ef36a0843da84f7b92f82bd57a8499f3
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-14 21:16:48 +0800

    Update PlayerData.js

--- roll/PlayerData.js
+++ roll/PlayerData.js
@@ -1,6 +1,6 @@
 var rply ={type : 'text'}; //type是必需的,但可以更改
 var GoogleSpreadsheet = require('google-spreadsheet');
-var creds = require('./client_secret.json');
+var creds = require('../client_secret.json');
 
 var DB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA/edit#gid=0');
 
