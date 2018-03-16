ff4b9de9b91c9fec2320fdc0b6bcb7fd4d4a6007
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-16 17:48:18 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -11,7 +11,7 @@ var creds = require('../client_secret.json');
 
 var CharDB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA');
 
-var CharArr[];
+var CharArr = [];
 
 DB.useServiceAccountAuth(creds, function (err) {
 		
