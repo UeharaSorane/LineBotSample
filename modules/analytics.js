6e6c9b64c1f434a96d3dfb17b936108ada3c9b20
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-17 14:33:25 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -18,7 +18,7 @@ var CharArr = new Array(5);
 
 //用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
 //格式是 exports.骰組檔案名字.function名
-function parseInput(rplyToken, inputStr,UserID) {
+function parseInput(rplyToken, inputStr,UserID,Dname) {
 	//console.log('InputStr: ' + inputStr);
 	_isNaN = function(obj) 	{
 	return isNaN(parseInt(obj));  
@@ -49,7 +49,7 @@ function parseInput(rplyToken, inputStr,UserID) {
 	if (trigger.match(/^uid$/)!= null ) return exports.Test.UserID(UserID);//資料庫連結測試
 	
 	////////////////////////////資料儲存相關
-	if (trigger.match(/^玩家情報$/)!= null ) return exports.PlayerData.main(UserID);//資料庫連結測試
+	if (trigger.match(/^玩家情報$/)!= null ) return exports.PlayerData.main(UserID,Dname);//資料庫連結測試
 	if (trigger.match(/^玩家建立$/)!= null ) return exports.PlayerData.CreatNewPlayer(UserID, mainMsg[1], mainMsg[2]);//建立新玩家
 	if (trigger.match(/^資料庫更新$/)!= null ) return exports.PlayerData.ArrayUpdate();//資料庫外部更動更新
 	if (trigger.match(/^繼承模式開啟$/)!= null ) return exports.PlayerData.InheritModeOn(mainMsg[1], mainMsg[2]);//[GM專用]開啟繼承權限
