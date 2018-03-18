7636fa11aa5dc68b00b4cf3e8567f75cb6abfdea
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-18 17:10:31 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -64,7 +64,7 @@ function parseInput(rplyToken, inputStr,UserID,Dname) {
 	
 
 	////////////////////////////服務相關
-	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return exports.PlayerData.BoxOpen(UserID) ;//寶箱狩獵指令
+	if (trigger.match(/^寶箱$|^開寶箱$/) != null) return exports.PlayerData.box(UserID) ;//寶箱狩獵指令
 
 	////////////////////////////娛樂相關
 
