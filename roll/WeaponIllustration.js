766b47c6a95783bd7c0441e476e7d63c6adc1d5e
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-18 19:57:24 +0800

    Update WeaponIllustration.js

--- roll/WeaponIllustration.js
+++ roll/WeaponIllustration.js
@@ -45,3 +45,58 @@ WeaponDB.useServiceAccountAuth(creds, function (err) {
 		
 		
 	});
+
+function WeapIllustration(Name){
+	for(var i = 0;i<WeapArr.length; i++){
+		if(WeapArr[i][0] == Name || WeapArr[i][1] == Name){
+			rply.text = '武器情報:\
+					\n-----基本資料-----\
+					\n 武器編號: ' + WeapArr[i][0] + '\
+					\n 武器名稱: ' + WeapArr[i][1] + '\
+					\n 稀有度: ' + WeapArr[i][2] + '\
+					\n 武器類型: ' + WeapArr[i][3] + '\
+					\n 武器被動: ' + WeapArr[i][4] + '\
+					\n-----能力值一覽-----\
+					\n 增加Hp: ' + WeapArr[i][5] + '\
+					\n 增加Mp: ' + WeapArr[i][6] + '\
+					\n 增加攻擊力: ' + WeapArr[i][7] + '\
+					\n-----武器描述-----\n' + WeapArr[i][8] + '\
+					\n-----取得途徑-----\n' + WeapArr[i][9] + '\
+					\n-----進化相關-----\
+					\n 向上進化: ' + WeapArr[i][10] + '\
+					\n-----進化樹-----\n' + WeapArr[i][12] + '\
+					\n---------------';
+			
+			return rply;
+		
+		}
+	}
+	if(Name == null){
+		rply.text = '-----武器圖鑑-----\n\n';
+		
+		for(var i = 0; i<=WeapArr.length; i++){
+			rply.text += '[' + WeapArr[i][0] + '] ' + WeapArr[i][1] + ' ('  + WeapArr[i][2] + ') [' + WeapArr[i][3] + ']\n';
+		}
+		
+		rply.text += '想要查詢特定武器的話，請輸入 武器圖鑑 武器編號(武器名字)';
+		
+		return rply;
+	}else{
+		
+		rply.text = '找不到編號或名稱為' + Name +'的武器喔！\
+				\n\n-----武器圖鑑-----\n\n';
+		
+		for(var i = 0; i<=WeapArr.length; i++){
+			rply.text += '[' + WeapArr[i][0] + '] ' + WeapArr[i][1] + ' ('  + WeapArr[i][2] + ') [' + WeapArr[i][3] + ']\n';
+		}
+		
+		rply.text += '想要查詢特定武器的話，請輸入 武器圖鑑 武器編號(武器名字)';
+	
+	}
+	
+
+}
+
+module.exports = {
+	WeapIllustration
+};
