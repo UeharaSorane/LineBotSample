5db84b92253a6e082c49c266674c81bf37390ebc
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-18 22:29:01 +0800

    Update AccessoriesIllustration.js

--- roll/AccessoriesIllustration.js
+++ roll/AccessoriesIllustration.js
@@ -20,22 +20,17 @@ AccessDB.useServiceAccountAuth(creds, function (err) {
 				for(var i=0; i< rows.length; i++){
 					WeapArr[i] = [];
 					
-					AccessArr[i][0] = rows[i].wid;
-					AccessArr[i][1] = rows[i].wname;
-					AccessArr[i][2] = rows[i].rare;
-					AccessArr[i][3] = rows[i].wtype;
-					AccessArr[i][4] = rows[i].ability;
-					AccessArr[i][5] = Number(rows[i].hp);
-					AccessArr[i][6] = Number(rows[i].mp);
-					AccessArr[i][7] = Number(rows[i].atk);
-					AccessArr[i][8] = rows[i].wdescription;
-					AccessArr[i][9] = rows[i].htgi;
-					AccessArr[i][10] = rows[i].evolution;
-					AccessArr[i][11] = rows[i].evolutionrate;
-					AccessArr[i][12] = rows[i].evolutiontree;
+					AccessArr[i][0] = rows[i].acid;
+					AccessArr[i][1] = rows[i].acname;
+					AccessArr[i][2] = rows[i].ability;
+					AccessArr[i][3] = Number(rows[i].hp);
+					AccessArr[i][4] = Number(rows[i].mp);
+					AccessArr[i][5] = Number(rows[i].atk);
+					AccessArr[i][6] = rows[i].acdescription;
+					AccessArr[i][7] = rows[i].htgi;
 					
 				}
-				//console.log(AccessArr);
+				console.log(AccessArr);
 				console.log('飾品資料 讀取完成');
 			}
 		
@@ -51,23 +46,18 @@ AccessDB.useServiceAccountAuth(creds, function (err) {
 function AccessoriesIllustration(Name){
 	for(var i = 0 ;i<WeapArr.length; i++){
 		if(AccessArr[i][0] == Name || AccessArr[i][1] == Name){
-			rply.text = '武器情報:\
+			rply.text = '飾品情報:\
 					\n-----基本資料-----\
-					\n 武器編號: ' + AccessArr[i][0] + '\
-					\n 武器名稱: ' + AccessArr[i][1] + '\
-					\n 稀有度: ' + AccessArr[i][2] + '\
-					\n 武器類型: ' + AccessArr[i][3] + '\
-					\n 武器被動: ' + AccessArr[i][4] + '\
-					\n' + Ability.AbilityReturn(AccessArr[i][4]) + '\
+					\n 飾品編號: ' + AccessArr[i][0] + '\
+					\n 飾品名稱: ' + AccessArr[i][1] + '\
+					\n 飾品被動: ' + AccessArr[i][2] + '\
+					\n' + Ability.AbilityReturn(AccessArr[i][2]) + '\
 					\n-----能力值一覽-----\
-					\n 增加Hp: ' + AccessArr[i][5] + '\
-					\n 增加Mp: ' + AccessArr[i][6] + '\
-					\n 增加攻擊力: ' + AccessArr[i][7] + '\
-					\n-----武器描述-----\n' + AccessArr[i][8] + '\
-					\n-----取得途徑-----\n' + AccessArr[i][9] + '\
-					\n-----進化相關-----\
-					\n 向上進化: ' + AccessArr[i][10] + '\
-					\n-----進化樹-----\n' + AccessArr[i][12] + '\
+					\n 增加Hp: ' + AccessArr[i][3] + '\
+					\n 增加Mp: ' + AccessArr[i][4] + '\
+					\n 增加攻擊力: ' + AccessArr[i][5] + '\
+					\n-----飾品描述-----\n' + AccessArr[i][6] + '\
+					\n-----取得途徑-----\n' + AccessArr[i][7] + '\
 					\n--------------------';
 			
 			return rply;
@@ -75,25 +65,25 @@ function AccessoriesIllustration(Name){
 		}
 	}
 	if(Name == null){
-		rply.text = '-----武器圖鑑-----\n\n';
+		rply.text = '-----飾品圖鑑-----\n\n';
 		
 		for(var i = 0; i<AccessArr.length; i++){
-			rply.text += '[' + AccessArr[i][0] + '] ' + AccessArr[i][1] + ' ('  + AccessArr[i][2] + ') [' + AccessArr[i][3] + ']\n';
+			rply.text += '[' + AccessArr[i][0] + '] ' + AccessArr[i][1];
 		}
 		
-		rply.text += '\n\n想要查詢特定武器的話，請輸入 武器圖鑑 武器編號(武器名字)';
+		rply.text += '\n\n想要查詢特定飾品的話，請輸入 飾品圖鑑 飾品編號(飾品名字)';
 		
 		return rply;
 	}
 		
-		rply.text = '找不到編號或名稱為' + Name +'的武器喔！\
-				\n\n-----武器圖鑑-----\n\n';
+		rply.text = '找不到編號或名稱為' + Name +'的飾品喔！\
+				\n\n-----飾品圖鑑-----\n\n';
 		
 		for(var i = 0; i<AccessArr.length; i++){
-			rply.text += '[' + AccessArr[i][0] + '] ' + AccessArr[i][1] + ' ('  + AccessArr[i][2] + ') [' + AccessArr[i][3] + ']\n';
+			rply.text += '[' + AccessArr[i][0] + '] ' + AccessArr[i][1];
 		}
 		
-		rply.text += '想要查詢特定武器的話，請輸入 武器圖鑑 武器編號(武器名字)';
+		rply.text += '\n\n想要查詢特定飾品的話，請輸入 飾品圖鑑 飾品編號(飾品名字)';
 		
 		return rply;
 }
