c9520078ec955944da048f4455c0f0f55d57ed4c
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-21 21:32:10 +0800

    Create WeaponBox.js

---
+++ roll/WeaponBox.js
@@ -0,0 +1,39 @@
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+///引入其他資料陣列
+var BattleStates = require('./BattleStates.js');
+var Weapons = require('./WeaponIllustration.js');
+///
+
+///引入資料庫
+
+var fs = require('fs');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('../client_secret.json');
+
+var DB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA');
+var WeaponBoxArr= [];
+
+DB.useServiceAccountAuth(creds, function (err) {
+		
+ 
+	
+ // 是先將資料讀進陣列
+	DB.getCells(12 , 
+		function (err, cells) {
+			if (err) {
+				console.log( err );
+			}else{
+
+				
+				console.log(cells);
+				//console.log('玩家所持武器庫 讀取完成');
+			}
+		
+
+			
+			});
+	
+		
+		
+	});
