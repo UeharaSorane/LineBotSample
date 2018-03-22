2e84aaaad185886330bbefbc4c3ba638d31812f7
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-22 10:30:42 +0800

    Update BadgeBox.js

--- roll/BadgeBox.js
+++ roll/BadgeBox.js
@@ -2,11 +2,11 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 
 ///引入其他資料陣列
 var BattleStates = require('./BattleStates.js');
-var Accessory = require('./AccessoriesIllustration.js');
+var Badge = require('./BadgeIllustration.js');
 var PlayerData = require('./PlayerData.js');
 
 var BattleStatesDataArray = BattleStates.GetArray();
-var WeaponsArray = Accessory.GetArray();
+var WeaponsArray = Badge.GetArray();
 ///
 
 ///引入資料庫
@@ -22,7 +22,7 @@ DB.useServiceAccountAuth(creds, function (err) {
 	var PlayerNumber = 0;
 	
  // 是先將資料讀進陣列
-	DB.getCells(13 , 
+	DB.getCells(14 , 
 		function (err, cells) {
 			if (err) {
 				console.log( err );
@@ -50,7 +50,7 @@ DB.useServiceAccountAuth(creds, function (err) {
 					}
 					
 				}
-				console.log('玩家所持武器庫 讀取完成');
+				console.log('玩家所持紋章庫 讀取完成');
 			}
 		
 
@@ -61,17 +61,17 @@ DB.useServiceAccountAuth(creds, function (err) {
 		
 	});
 
-function SearchAccessory(UserID){
+function SearchBadge(UserID){
 	for(var i =0; i<WeaponBoxArr.length;i++){
 		if(WeaponBoxArr[i][0] == UserID){
 			for(var j = 0; j<BattleStatesDataArray.length;j++){
 				if(BattleStatesDataArray[j][0] == UserID){
 					rply.text = '玩家 ' + BattleStatesDataArray[j][1] + '\n\
-								\n 目前裝備飾品: ' + BattleStatesDataArray[j][5] + '持有飾品一覽:\n';
+								\n 目前裝備紋章: ' + BattleStatesDataArray[j][7] + '\n持有紋章一覽:\n';
 					for(var k = 1; k<WeaponBoxArr[i].length; k++){
 						rply.text += WeaponBoxArr[i][k] + '\n';
 					}
-					rply.text += '\n 想更換飾品的話，請輸入 飾品更換 要裝備的飾品名';
+					rply.text += '\n 想更換紋章的話，請輸入 紋章更換 要裝備的飾品名';
 					
 					return rply;
 					
@@ -81,43 +81,30 @@ function SearchAccessory(UserID){
 		}
 	}
 
-	rply.text = '找不到你的角色的飾品庫，請向GM確認';
+	rply.text = '找不到你的角色的紋章庫，請向GM確認';
 	return rply;
 
 }
 
-function SwitchAccess(UserID,Accessory){
+function SwitchBadge(UserID,Badge){
 	for(var i =0; i<WeaponBoxArr.length;i++){
 		if(WeaponBoxArr[i][0] == UserID){
 			for(var j = 0; j<BattleStatesDataArray.length;j++){
 				if(BattleStatesDataArray[j][0] == UserID){
 					rply.text = '玩家 ' + BattleStatesDataArray[j][1] + '\n\
-								\n 目前裝備飾品: ' + BattleStatesDataArray[j][5] + '\n';
+								\n 目前裝備飾品: ' + BattleStatesDataArray[j][7] + '\n';
 					for(var k = 1; k<WeaponBoxArr[i].length; k++){
-						if(WeaponBoxArr[i][k] == Accessory){
+						if(WeaponBoxArr[i][k] == Badge){
 							for(var l =0; l<WeaponsArray.length; l++){
-								if(WeaponsArray[l][1] == Accessory){
+								if(WeaponsArray[l][1] == Badge){
 									
 									rply.text += '更換成: ' + WeaponsArray[l][1] + '\n\
 											\n -----能力值變動-----\
-											\n 飾品被動: ' + BattleStatesDataArray[j][6] + '->' + WeaponsArray[l][2] + '\
-											\n Hp:' + (BattleStatesDataArray[i][9] + BattleStatesDataArray[i][10]*10 + BattleStatesDataArray[i][11] + BattleStatesDataArray[i][12] + BattleStatesDataArray[i][13]);
-									BattleStatesDataArray[j][12] = WeaponsArray[l][3];
-									rply.text += '->' + (BattleStatesDataArray[i][9] + BattleStatesDataArray[i][10]*10 + BattleStatesDataArray[i][11] + BattleStatesDataArray[i][12] + BattleStatesDataArray[i][13]) + '\n\
-											\n Mp:' + (BattleStatesDataArray[i][14] + BattleStatesDataArray[i][15]*5 + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17] + BattleStatesDataArray[i][18]);
-									
-									BattleStatesDataArray[j][17] = WeaponsArray[l][4];
-									
-									rply.text+= '->' + (BattleStatesDataArray[i][14] + BattleStatesDataArray[i][15]*5 + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17] + BattleStatesDataArray[i][18]) +'\n\
-											\n Atk:' + (BattleStatesDataArray[i][19] + BattleStatesDataArray[i][20] + BattleStatesDataArray[i][21] + BattleStatesDataArray[i][22] + BattleStatesDataArray[i][23]);
-									BattleStatesDataArray[j][22] = WeaponsArray[l][5];
-									
-									rply.text += '->' + (BattleStatesDataArray[i][19] + BattleStatesDataArray[i][20] + BattleStatesDataArray[i][21] + BattleStatesDataArray[i][22] + BattleStatesDataArray[i][23]) +'\n\
+											\n 紋章被動: ' + BattleStatesDataArray[j][7] + '->' + WeaponsArray[l][1] + '\
 									\n-------------------------';
 									
 									
-									BattleStatesDataArray[j][5] = WeaponsArray[l][1];
-									BattleStatesDataArray[j][6] = WeaponsArray[l][2];
+									BattleStatesDataArray[j][7] = WeaponsArray[l][1];
 									
 									BattleStates.saveArray(BattleStatesDataArray);
 									
@@ -128,17 +115,17 @@ function SwitchAccess(UserID,Accessory){
 							
 							}
 							
-							rply.text += '\n 錯誤！你持有圖鑑沒有的飾品，請回報給GM確認';
+							rply.text += '\n 錯誤！你持有圖鑑沒有的紋章，請回報給GM確認';
 							return rply;
 							
 						}
 					}
-					if(Accessory == null){
-						rply.text = '請輸入你要裝備的飾品！';
+					if(Badge == null){
+						rply.text = '請輸入你要裝備的紋章！';
 						return rply;
 					}
 					
-					rply.text = '你尚未擁有飾品' + Accessory + '喔！';
+					rply.text = '你尚未擁有紋章' + Badge + '喔！';
 					return rply;
 					
 				}
@@ -151,12 +138,12 @@ function SwitchAccess(UserID,Accessory){
 		}
 	}
 
-	rply.text = '找不到你的角色的飾品庫，請向GM確認';
+	rply.text = '找不到你的角色的紋章庫，請向GM確認';
 	return rply;
 	
 }
 
 module.exports = {
-	SearchAccessory,
-	SwitchAccess
+	SearchBadge,
+	SwitchBadge
 };
