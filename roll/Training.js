b0f2b02b7c7932d21faa8e46d50f4949efacfa8f
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-23 22:34:01 +0800

    Update Training.js

--- roll/Training.js
+++ roll/Training.js
@@ -1,1 +1,9 @@
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+///引入其他資料陣列
+var BattleStates = require('./BattleStates.js');
+var PlayerData = require('./PlayerData.js');
+
+var BattleStatesDataArray = BattleStates.GetArray();
+///
 
