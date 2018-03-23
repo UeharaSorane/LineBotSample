1d856cc9b3296a4bf32a621f89df5f1d541333a4
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-23 23:29:25 +0800

    Update Training.js

--- roll/Training.js
+++ roll/Training.js
@@ -9,6 +9,8 @@ var BattleStatesDataArray = BattleStates.GetArray();
 
 function TrainingStates(UserID){
 	
+	BattleStatesDataArray = BattleStates.GetArray();
+	
 	for(var i =0; i<BattleStatesDataArray.length;i++){
 		if(UserID == BattleStatesDataArray[i][0]){
 			rply.text ='玩家 ' + BattleStatesDataArray[i][1] + '的訓練情報:\n\
@@ -18,7 +20,7 @@ function TrainingStates(UserID){
 					\n----------\
 					\n Mp基本值: ' + BattleStatesDataArray[i][14] + '\
 					\n Mp訓練等級: ' + BattleStatesDataArray[i][15] + '(+' + BattleStatesDataArray[i][15]*5 + ')\
-					\n Mp總和值: ' + (BattleStatesDataArray[i][14] + BattleStatesDataArray[i][14]*5 + BattleStatesDataArray[i][15] + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17]) + '\
+					\n Mp總和值: ' + (BattleStatesDataArray[i][14] + BattleStatesDataArray[i][15]*5 + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17] + BattleStatesDataArray[i][18]) + '\
 					\n----------\
 					\n Atk基本值: ' + BattleStatesDataArray[i][19] + '\
 					\n Atk訓練等級: ' + BattleStatesDataArray[i][20] + '(+' + BattleStatesDataArray[i][20] + ')\
@@ -41,6 +43,72 @@ function TrainingStates(UserID){
 
 }
 
+function setTrain(UserID,Hp,Mp,Atk){
+	BattleStatesDataArray = BattleStates.GetArray();
+	
+	let HpTemp,MpTemp,AtkTemp;
+	
+	if(Hp == null) HpTemp = 0;
+	else HpTemp = Hp;
+	
+	if(Mp == null) MpTemp = 0;
+	else MpTemp = Mp;
+	
+	if(Atk == null) AtkTemp = 0;
+	else AtkTemp = Atk;
+	
+	for(var i =0; i<BattleStatesDataArray.length;i++){
+		if(UserID == BattleStatesDataArray[i][0]){
+			
+			if(HpTemp + MpTemp + AtkTemp > BattleStatesDataArray[i][31]){
+				rply.text = '錯誤！你的訓練點數不足';
+				return rply;
+			}
+			
+			
+			
+			rply.text ='玩家 ' + BattleStatesDataArray[i][1] + ':\n分配結果\
+					\n Hp基本值: ' + BattleStatesDataArray[i][9] + '\
+					\n Hp訓練等級: ' + BattleStatesDataArray[i][10] + '(+' + BattleStatesDataArray[i][10]*10 + ') -> ' + HpTemp + '(+' + HpTemp*10 + ')\
+					\n Hp總和值: ' + (BattleStatesDataArray[i][9] + BattleStatesDataArray[i][10]*10 + BattleStatesDataArray[i][11] + BattleStatesDataArray[i][12] + BattleStatesDataArray[i][13])
+						+ ' -> ' + (BattleStatesDataArray[i][9] + HpTemp*10 + BattleStatesDataArray[i][11] + BattleStatesDataArray[i][12] + BattleStatesDataArray[i][13]) '\
+					\n----------\
+					\n Mp基本值: ' + BattleStatesDataArray[i][14] + '\
+					\n Mp訓練等級: ' + BattleStatesDataArray[i][15] + '(+' + BattleStatesDataArray[i][15]*5 + ') -> ' + MpTemp + '(+' + MpTemp*5 + ')\
+					\n Mp總和值: ' + (BattleStatesDataArray[i][14] + BattleStatesDataArray[i][15]*5 + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17] + BattleStatesDataArray[i][18])
+						+ ' -> ' + (BattleStatesDataArray[i][14] + MpTemp*5 + BattleStatesDataArray[i][16] + BattleStatesDataArray[i][17] + BattleStatesDataArray[i][18]) '\
+					\n----------\
+					\n Atk基本值: ' + BattleStatesDataArray[i][19] + '\
+					\n Atk訓練等級: ' + BattleStatesDataArray[i][20] + '(+' + BattleStatesDataArray[i][20] + ') -> ' + AtkTemp + '(+' + AtkTemp + ')\
+					\n Atk總和值: ' + (BattleStatesDataArray[i][19] + BattleStatesDataArray[i][20] + BattleStatesDataArray[i][21] + BattleStatesDataArray[i][22] + BattleStatesDataArray[i][23])
+						+ ' -> ' + (BattleStatesDataArray[i][19] + AtkTemp + BattleStatesDataArray[i][21] + BattleStatesDataArray[i][22] + BattleStatesDataArray[i][23]) '\
+					\n----------\
+					\n\
+					\n 訓練等級: ' + BattleStatesDataArray[i][31] + '等\
+					\n 尚未分配的訓練點數: ' + BattleStatesDataArray[i][30] + '點 -> ' + (BattleStatesDataArray[i][31] - HpTemp - MpTemp - AtkTemp) + '點\
+					\n\
+					\n --------------';
+			
+			BattleStatesDataArray[i][10] = HpTemp;
+			BattleStatesDataArray[i][15] = MpTemp;
+			BattleStatesDataArray[i][20] = AtkTemp;
+			BattleStatesDataArray[i][30] = BattleStatesDataArray[i][31] - HpTemp - MpTemp - AtkTemp;
+			
+			BattleStates.saveArray(BattleStatesDataArray);
+			return rply;
+			
+			
+		}
+	}
+	
+	rply.text = '找不到你的角色的戰鬥資料，請向GM確認';
+	return rply;
+	
+	
+}
+
+
 module.exports = {
-	TrainingStates
+	TrainingStates,
+	setTrain
 };
