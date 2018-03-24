0fdf0706483bb1c433c429269952d6757d79d019
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-24 15:06:12 +0800

    Update BattleStates.js

--- roll/BattleStates.js
+++ roll/BattleStates.js
@@ -7,7 +7,7 @@ var DB = new GoogleSpreadsheet('12y_EgRKvjO7a1xEc5wbM5ERofFfXW-csoR4_R0H0HfA');
 var CharArr= [];
 var WeaponI = require('./WeaponIllustration.js');
 
-var WeaponsArr = WeaponI.getArray();
+var WeaponsArr = WeaponI.GetArray();
 
 DB.useServiceAccountAuth(creds, function (err) {
 		
