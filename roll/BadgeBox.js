9d4a4eef375cafe010b612d5470a11d0cf0183cf
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-24 16:05:04 +0800

    Update BadgeBox.js

--- roll/BadgeBox.js
+++ roll/BadgeBox.js
@@ -78,6 +78,7 @@ function UpdateArray(){
 }
 
 function SearchBadge(UserID){
+	UpdateArray();
 	for(var i =0; i<WeaponBoxArr.length;i++){
 		if(WeaponBoxArr[i][0] == UserID){
 			for(var j = 0; j<BattleStatesDataArray.length;j++){
