d6d75162206d12aba643c480a409ee0fccb8dfeb
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-24 16:01:26 +0800

    Update BadgeBox.js

--- roll/BadgeBox.js
+++ roll/BadgeBox.js
@@ -164,7 +164,16 @@ function CreatNewPlayer(UserID,STWeapon){
 	WeaponBoxArr[WeaponBoxArr.length] = [];
 	WeaponBoxArr[WeaponBoxArr.length-1][0] = UserID;
 	WeaponBoxArr[WeaponBoxArr.length-1][1] = [STWeapon];
-	UpdateArray();
+	DB.useServiceAccountAuth(creds, function (err) {
+ 
+	  // Get all of the rows from the spreadsheet.
+	  DB.addRow(14, { Userid: UserID}, function(err) {
+		  if(err) {
+		    console.log(err);
+		  }
+		  
+		});
+	});
 	
 }
 
