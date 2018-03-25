de70e0b9d7cf2ea286b66c57244cbbb5b5efe9fa
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-25 16:48:09 +0800

    Update MateShop.js

--- roll/MateShop.js
+++ roll/MateShop.js
@@ -6,6 +6,7 @@ var PlayerData = require('./PlayerData.js');
 var WeaponBox = require('./WeaponBox.js');
 var BadgeBox = require('./BadgeBox.js');
 var MateBox = require('./MateBox.js');
+var ItemBox = require('./ItemBox.js');
 
 var PD = PlayerData.GetArray();
 var WD = WeaponBox.GetArray();
@@ -33,7 +34,7 @@ ShopDB.useServiceAccountAuth(creds, function (err) {
 					ShopArr[i][0] = rows[i].goodid;
 					ShopArr[i][1] = rows[i].name;
 					ShopArr[i][2] = rows[i].type;
-					ShopArr[i][3] = rows[i].cost;
+					ShopArr[i][3] = Number(rows[i].cost);
 					ShopArr[i][4] = rows[i].weapon;
 					ShopArr[i][5] = rows[i].accessory;
 					ShopArr[i][6] = rows[i].badge;
@@ -78,11 +79,82 @@ function MateShop(UserID,Goods,confirm){
 			}
 			for(var k = 0;k<ShopArr.length;k++){
 				if(ShopArr[k][1] == Goods){
+					if(PD[i][13]<ShopArr[k][3]){
+						rply.text = '錯誤！夥伴碎片不足' +(PD[i][13]-ShopArr[k][3]);
+						return rply;
+					}
+					
 					if(confirm != '確定'){
 						rply.text = '\n[' + ShopArr[k][0] +  '] ' + ShopArr[k][1] + ' (' + ShopArr[k][2] + ') -' + ShopArr[k][3] + '夥伴碎片\n\
 							\n' + ShopArr[k][15] + '\n\
 							\n 持有夥伴碎片: ' + PD[i][13] + '\
 							\n確定購買的話，請輸入 夥伴商店 道具名 確定 完成手續';
+						return rply;
+					}else{
+						if(ShopArr[k][2] == '武器'){
+							for(var l = 0;l<WD.length;l++){
+								if(WD[l][0] == UserID){
+									for(var m =0;m<WD[l][2].length; m++){
+										if(WD[l][m] == Goods){
+											rply.text = '錯誤！你已經擁有' + Goods + '了';
+											return rply;
+										}
+									}
+									WD.getWeapon(UserID,Goods);
+								}
+							}
+						}else if(ShopArr[k][2] == '飾品'){
+							for(var l = 0;l<AD.length;l++){
+								if(AD[l][0] == UserID){
+									for(var m =0;m<AD[l][2].length; m++){
+										if(AD[l][m] == Goods){
+											rply.text = '錯誤！你已經擁有' + Goods + '了';
+											return rply;
+										}
+									}
+									require('./AccessoryBox.js').getAcce(UserID,Goods);
+								}
+							}
+						}else if(ShopArr[k][2] == '紋章'){
+							for(var l = 0;l<BD.length;l++){
+								if(BD[l][0] == UserID){
+									for(var m =0;m<BD[l][2].length; m++){
+										if(BD[l][m] == Goods){
+											rply.text = '錯誤！你已經擁有' + Goods + '了';
+											return rply;
+										}
+									}
+									BadgeBox.getBadge(UserID,Goods);
+								}
+							}
+						}else if(ShopArr[k][2] == '夥伴'){
+							for(var l = 0;l<MD.length;l++){
+								if(MD[l][0] == UserID){
+									for(var m =0;m<MD[l][2].length; m++){
+										if(MD[l][m] == Goods){
+											rply.text = '錯誤！你已經擁有' + Goods + '了';
+											return rply;
+										}
+									}
+									let Temp = [Goods];
+									MateBox.getMate(UserID,Temp);
+								}
+							}
+						}else if(ShopArr[k][2] == '道具'){
+							ItemBox.getItem(UserID,Goods);
+						}else if(ShopArr[k][2] == '素材'){
+							PD[i][7]+=ShopArr[k][9];
+							PD[i][8]+=ShopArr[k][10];
+							PD[i][9]+=ShopArr[k][11];
+							PD[i][10]+=ShopArr[k][12];
+							PD[i][11]+=ShopArr[k][13];
+							PD[i][12]+=ShopArr[k][14];
+						}
+						
+						PD[i][13]-=ShopArr[k][3];
+						PlayerData.saveArray(PD);
+						
+						rply.text = '你成功購買了' + Goods + '！';
 						return rply;
 					}
 				}
