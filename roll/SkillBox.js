b560dfc4edeec38a18b6d7aa33ef81c10baef545
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-26 08:19:53 +0800

    Update SkillBox.js

--- roll/SkillBox.js
+++ roll/SkillBox.js
@@ -84,6 +84,7 @@ function SearchSkill(UserID){
 	UpdateArray();
 	for(var i =0; i<WeaponBoxArr.length;i++){
 		if(WeaponBoxArr[i][0] == UserID){
+			console.log(WeaponBoxArr[i][2]);
 			for(var j = 0; j<BattleStatesDataArray.length;j++){
 				if(BattleStatesDataArray[j][0] == UserID){
 					rply.text = '玩家 ' + BattleStatesDataArray[j][1] + '\n\
