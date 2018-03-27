02833aaea55855d49d6dec3bfecf8d22becf67d8
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-27 20:44:45 +0800

    Update Guild.js

--- roll/Guild.js
+++ roll/Guild.js
@@ -1,4 +1,5 @@
 var rply ={type : 'text'}; //type是必需的,但可以更改
+var returnRply;
 var fs = require('fs');
 var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('../client_secret.json');
@@ -80,28 +81,28 @@ function ArrayUpdate() {
 }
 
 function guildView(){
-	rply.text = '【公會一覽】';
+	returnRply = '【公會一覽】';
 	
 	for(var i  =0; i<CharArr.length;i++){
-		rply.text+= '\n [' + CharArr[i][0] + '] ' + CharArr[i][1] + ' (' + CharArr[i][5] + '人) [' + CharArr[i][6] + ']';
+		returnRply+= '\n [' + CharArr[i][0] + '] ' + CharArr[i][1] + ' (' + CharArr[i][5] + '人) [' + CharArr[i][6] + ']';
 	}
 	
-	rply.text += '\n 如果想要確認公會詳細內容，請輸入 公會 公會名(公會編號) 進行查詢';
+	returnRply += '\n 如果想要確認公會詳細內容，請輸入 公會 公會名(公會編號) 進行查詢';
 	
 	ArrayUpdate();
 	
-	return rply.text;
+	return returnRply;
 }
 
 function guildSearch(GuildName){
 	for(var i =0; i<CharArr.length;i++){
 		if(CharArr[i][1] == '輔導公會'){
-			rply.text = '【輔導公會】 無公會玩家的歸宿\
+			returnRply = '【輔導公會】 無公會玩家的歸宿\
 					\n 只要你沒有加入公會，就會來到這裡！';
-			return rply.text;
+			return returnRply;
 			
 		}else if(CharArr[i][1] == GuildName || CharArr[i][0] == GuildName){
-			rply.text = '【公會情報】\
+			returnRply = '【公會情報】\
 					\n 公會編號: ' + CharArr[i][0] + '\
 					\n 公會名: ' + CharArr[i][1] + '\
 					\n 公會會長: ' + CharArr[i][3][0] + '\
@@ -110,39 +111,25 @@ function guildSearch(GuildName){
 					\n-----公會成員-----';
 			
 			for(var j = 0;j<CharArr[i][3].length; j++){
-				rply.text += '\n' + CharArr[i][4][j] + ': ' + CharArr[i][3][j];
+				returnRply += '\n' + CharArr[i][4][j] + ': ' + CharArr[i][3][j];
 			}
 			
 			if(CharArr[i][6] == 'GM限定'){
-				rply.text += '\n\n這是GM限定公會，一般人是加不進來的';
+				returnRply += '\n\n這是GM限定公會，一般人是加不進來的';
 			}else if(CharArr[i][6] == '自由加入'){
-				rply.text += '\n\n這是可以自由加入公會，請輸入 公會 公會名 加入 就能直接加入公會了';
+				returnRply += '\n\n這是可以自由加入公會，請輸入 公會 公會名 加入 就能直接加入公會了';
 			}else if(CharArr[i][6] == '審核'){
-				rply.text += '\n\n這是必須審核才能加入的公會，請輸入 公會 公會名 加入 並等待公會會長同意';
+				returnRply += '\n\n這是必須審核才能加入的公會，請輸入 公會 公會名 加入 並等待公會會長同意';
 			}else if(CharArr[i][6] == '暫停招生'){
-				rply.text += '\n\n目前這個公會暫時停止招募新人了';
+				returnRply += '\n\n目前這個公會暫時停止招募新人了';
 			}
 			
-			return rply.text;
+			return returnRply;
 			
 		}
 	}
 	
 	return guildView();
-	
-}
-
-function GuildCommand(UserID,Guild,Command){
-	if(Guild == null){
-		rply.text = guildView();
-		return rply;
-	}else{
-		//if(Command == '加入'){}
-		if(Command == null){
-			rply.text = guildSearch(Guild);
-			return rply;
-		}
-	}
 	
 }
 
@@ -176,8 +163,8 @@ function switchName(UserID,Guild,Name){
 
 module.exports = {
 	guildView,
+	guildSearch,
 	GetArray,
 	InheritPlayer,
-	switchName,
-	GuildCommand
+	switchName
 };
