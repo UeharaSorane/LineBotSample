a1fd89c3265639fc9300dd76ac5a46a9b86eef51
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-28 22:01:28 +0800

    Update PlayerData.js

--- roll/PlayerData.js
+++ roll/PlayerData.js
@@ -640,7 +640,7 @@ function GuildInformation(UserID,command,guild){
 								if(GB[j][7][k] == CharArr[i][1]){
 									//console.log(GB[j][7][k]);
 									console.log(k);
-									console.log(GB[j][7].splice(k,1));
+									console.log(GB[j][7].splice(1,1));
 									
 									/*CharArr[i][16] = 0;
 									GB[j][7].splice(k,1);
