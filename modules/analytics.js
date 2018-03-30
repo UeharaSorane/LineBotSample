bdd9508462361c4196cc248d988879113cc824ef
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-30 22:03:20 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -94,7 +94,7 @@ function parseInput(rplyToken, inputStr,UserID,Dname) {
 	if (trigger.match(/^更名$/)!= null ) return exports.PlayerData.switchName(UserID, mainMsg[1]);//更名
 	if (trigger.match(/^更換稱號$/)!= null ) return exports.PlayerData.switchTitle(UserID, mainMsg[1]);//更換稱號
 	if (trigger.match(/^公會$/)!= null ) return exports.PlayerData.GuildInformation(UserID, mainMsg[1], mainMsg[2]);//公會功能
-	if (trigger.match(/^公會管理$/)!= null ) return exports.PlayerData.GuildManage(UserID, mainMsg[1], mainMsg[2]);//公會管理功能(會長限定)
+	if (trigger.match(/^公會管理$/)!= null ) return exports.PlayerData.GuildManage(UserID, mainMsg[1], mainMsg[2], mainMsg[3]);//公會管理功能(會長限定)
 	
 	////////////////////////////戰鬥資料相關
 	if (trigger.match(/^戰鬥能力$/)!= null ) return exports.BattleStates.BattleStates(UserID);//玩家情報
