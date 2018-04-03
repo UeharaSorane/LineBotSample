bd17972d08915411dd88125bb0e69a8c09be6e80
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-04-03 21:34:05 +0800

    Update GuildFacility.js

--- roll/GuildFacility.js
+++ roll/GuildFacility.js
@@ -23,23 +23,23 @@ DB.useServiceAccountAuth(creds, function (err) {
 					
 					CharArr[i][0] = rows[i].guildid;
 					CharArr[i][1] = rows[i].guildname;
-          CharArr[i][2] = Number(rows[i].trainlv);
-          CharArr[i][3] = Number(rows[i].factorylv);
-          CharArr[i][4] = Number(rows[i].shoplv);
-          CharArr[i][5] = Number(rows[i].restaurantlv);
-          CharArr[i][6] = Number(rows[i].warehouselv);
-          CharArr[i][7] = Number(rows[i].wms);
-          CharArr[i][8] = Number(rows[i].wmm);
-          CharArr[i][9] = Number(rows[i].wml);
-          CharArr[i][10] = Number(rows[i].gms);
-          CharArr[i][11] = Number(rows[i].gmm);
-          CharArr[i][12] = Number(rows[i].gml);
-          CharArr[i][13] = Number(rows[i].gold);
-          CharArr[i][14] = Number(rows[i].mirastone);
+					CharArr[i][2] = Number(rows[i].trainlv);
+					CharArr[i][3] = Number(rows[i].factorylv);
+					CharArr[i][4] = Number(rows[i].shoplv);
+					CharArr[i][5] = Number(rows[i].restaurantlv);
+				  	CharArr[i][6] = Number(rows[i].warehouselv);
+					CharArr[i][7] = Number(rows[i].wms);
+					CharArr[i][8] = Number(rows[i].wmm);
+					CharArr[i][9] = Number(rows[i].wml);
+					CharArr[i][10] = Number(rows[i].gms);
+					CharArr[i][11] = Number(rows[i].gmm);
+					CharArr[i][12] = Number(rows[i].gml);
+					CharArr[i][13] = Number(rows[i].gold);
+					CharArr[i][14] = Number(rows[i].mirastone);
 
 					
 				}
-				//console.log(CharArr);
+				console.log(CharArr);
 				console.log('公會設施資料 讀取完成');
 				//console.log();
 			}
@@ -81,20 +81,16 @@ function ArrayUpdate() {
 						rows[i].gmm = CharArr[i][11];
 						rows[i].gml = CharArr[i][12];
 						rows[i].gold = CharArr[i][13];
-            rows[i].mirastone = CharArr[i][14];
+						rows[i].mirastone = CharArr[i][14];
 						rows[i].save();
 						
 
 					}
 
-					}
-					console.log('公會設施資料 更新完成');
+				}
+				console.log('公會設施資料 更新完成');
+			});
 
-
-				});
-
-
-
-		});
+	});
 	
 }
