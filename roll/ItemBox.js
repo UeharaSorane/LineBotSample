a48fc20d744bf3a7b453913e745568aabf74dc55
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-04-06 22:29:37 +0800

    Update ItemBox.js

--- roll/ItemBox.js
+++ roll/ItemBox.js
@@ -231,8 +231,10 @@ function CreatNewPlayer(UserID,cname,STWeapon){
 	  // Get all of the rows from the spreadsheet.
 	  DB.addRow(19, { Userid: UserID}, function(err) {
 		  if(err) {
-		    console.log(err);
-		  }
+			console.log(err);
+		}else{
+			UpdateArray();
+		}
 		  
 		});
 	});
