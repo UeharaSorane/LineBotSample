329b6886d1015896a7b281afa713feacee78ccc3
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-02 19:23:47 +0800

    Update Testing.js

--- roll/Testing.js
+++ roll/Testing.js
@@ -6,9 +6,9 @@ var BattileUI = require('../battlesys/battleUI.js');
 var BattleInfo = BattileUI.BattleInfo;
 
 var bot = linebot({
-	channelId: "1568211787",
-	channelSecret: "9848b4e4a814dd07e84212da82bad4a3",
-	channelAccessToken: "68lsiTjqwl30Ff6Ik5NH7KYW4FGyRsZYV+NZcVsS85CU3o+QaiLETQNZgaZwYNKkdX71iUjw4pz1OfJCkJeovBzfvF2Eoit0anKv9/Mxxd6OEY06r20Ad6Lxi3rdadSKqE8khBgJZUYxQDbLndtoBQdB04t89/1O/w1cDnyilFU=",
+	channelId: "1635505649",
+	channelSecret: "b59dc842309386b494bb05541e10dfb3",
+	channelAccessToken: "jCV9Fe8nB3+G91MXq2eWCz5v2w+jGsE8A+kVdln0CF3E53aW5nZnzNfnlzkmVgZkf2OAxNahvylD0Z0VJ+wGDrDkfoyMQMvm525qu2T1c3h9FeL8VMMEwbTRXTEtUYO8Bfu5x0xYMwX/aoKJSHYrEAdB04t89/1O/w1cDnyilFU="
 });
 
 function ReplyTest(UserN,myText) {
@@ -55,40 +55,10 @@ function PushTest(UserID,GroupID) {
 
 }
 
-function SimBattle(UserID,GroupID,UserN){
-	var FightInfo = [];
-	
-	FightInfo[0] = 0;//是否在群組內遊玩
-	FightInfo[1] = 0;//群組ID
-	
-	if(GroupID != null){
-		FightInfo[0] = 1;
-		FightInfo[1] = GroupID;
-	}else{
-		rply[0] = 'rply';
-		rply[1] = '請在群組內戰鬥';
-		return rply;
-	}
-	FightInfo[2] = 1;//戰鬥類型
-	
-	FightInfo[3] = [];//戰鬥員情報
-	FightInfo[3][0] = new BattleInfo(UserID,UserN,'測試用玩家','player',100,1000,4,10,'無','木劍','無','無','無',['簡單治癒術','水平四方切','火球'],'未來之力-嵐刃','嵐刃-巨神懲罰者');
-	//FightInfo[3][1] = new BattleInfo('c','電腦','測試用電腦','player'150,200,1,20,'無','木劍','無','無','無',['簡單廣域治癒術','魔力暴風','無'],'無','無','無');
-	FightInfo[3][1] = new BattleInfo('c','電腦2','測試用電腦2','computer',150,20,1,10,'無','木劍','無','無','無',['火球','簡單廣域治癒術','無'],'無','無','無');
-	//FightInfo[3][2] = new BattleInfo('c','電腦3','測試用電腦3','computer',120,100,1,20,'無','木劍','無','無','無',['火球','水平四方切','魔力暴風'],'無','未來之力-嵐刃');
-	
-	FightInfo[4] = '測試用戰鬥';//戰鬥名稱
-	
-	FightInfo[5] = ['player','computer'];
-	
-	rply[0] = 'battleOn';
-	rply[1] = FightInfo;
-	return rply;
-}
+
 
 module.exports = {
 	ReplyTest,
 	PushTest,
-	SimBattle,
 	SecretTalk
 };
