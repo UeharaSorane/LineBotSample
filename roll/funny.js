c77c7b5963e6a00a74ce2d42f04c296a75e40d0d
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-03 20:20:49 +0800

    Update funny.js

--- roll/funny.js
+++ roll/funny.js
@@ -97,7 +97,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 function randomLuck(TEXT) {
 	let rplyArr = ['超吉','超級上吉','大吉','吉','中吉','小吉','吉','小吉','吉','吉','中吉','吉','中吉','吉','中吉','小吉','末吉','吉','中吉','小吉','末吉','中吉','小吉','小吉','吉','小吉','末吉','中吉','小吉','凶','小凶','沒凶','大凶','很凶','你不要知道比較好呢','命運在手中,何必問我'];
 	rply.text = TEXT[0] + ' ： ' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-	return rply;
+	return ['rply',rply];
 }
 ////////////////
 
