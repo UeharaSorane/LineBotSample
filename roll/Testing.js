fa2087a5b3a5958eaf4bac8095a06820622c973f
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-03 22:07:21 +0800

    Update Testing.js

--- roll/Testing.js
+++ roll/Testing.js
@@ -7,10 +7,44 @@ var bot = linebot({
   channelAccessToken: "jCV9Fe8nB3+G91MXq2eWCz5v2w+jGsE8A+kVdln0CF3E53aW5nZnzNfnlzkmVgZkf2OAxNahvylD0Z0VJ+wGDrDkfoyMQMvm525qu2T1c3h9FeL8VMMEwbTRXTEtUYO8Bfu5x0xYMwX/aoKJSHYrEAdB04t89/1O/w1cDnyilFU="
 });
 
-var talkChannal = [
-	"C7dea53a651793073f816c1838e6eb69d",//trpg群
-	"U7783ccf51debc249a07bc1d957bd3a69"//兔司機
-];
+var rply ={type : 'text'}; //type是必需的,但可以更改
+var Ability = require('./Ability.js');
+var fs = require('fs');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('../client_secret.json');
+
+var ChannalDB = new GoogleSpreadsheet('1hwFlTrJ7JHeWMLbHmfg7LP7f13OfAoMebF6HIkHpHPs');
+var talkChannal= [];
+
+ChannalDB.useServiceAccountAuth(creds, function (err) {
+		
+ 
+	
+ // 是先將資料讀進陣列
+	ChannalDB.getRows(5 , 
+		function (err, rows) {
+			if (err) {
+				console.log( err );
+			}else{
+				for(var i=0; i< rows.length; i++){
+					talkChannal[i] = [];
+					
+					talkChannal[i][0] = rows[i].channalname;
+					talkChannal[i][1] = rows[i].id;
+					talkChannal[i][2] = rows[i].descripition;
+					
+				}
+				//console.log(AccessArr);
+				console.log('頻道資料 讀取完成');
+			}
+		
+
+			
+			});
+	
+		
+		
+	});
 
 function ReplyTest(UserN,myText) {
 	///確認系統reply功能沒問題用
@@ -30,17 +64,31 @@ function ReplyTest(UserN,myText) {
 
 function SecretTalk(UserID,Channal,myText) {
 	///匿名淦話
-	rply[0] = 'push';
-	if(talkChannal[Channal-1] != null){
-		rply[0] = 'push';
-		bot.push(talkChannal[Channal-1],myText);
-		return rply;
-	}
-	else{
+	if(Channal == null){
 		rply[0] = 'rply';
-		rply[1] = '找不到頻道喔';
+		rply[1] = '這是目前有登錄的頻道清單：\';
+		for(var a = 0;a< talkChannal.length;a++){
+			rply[1] += '\n'+(a+1) +'\.'+talkChannal[a][0];
+		}
+		rply[1] += '\n\n 想要進行匿名對話，請輸入[淦話 頻道編號 對話內容]';
+		        return rply;
+	}else if(talkChannal[Channal] == null){
+		rply[0] = 'rply';
+		rply[1] = '找不到該頻道喔';
 		return rply;
 		
+	}else{
+		if(myText == null){
+			rply[0] = 'rply';
+		        rply[1] = '頻道名稱:'+ talkChannal[Channal][0]+'\
+                                \n 描述:'+ talkChannal[Channal][2]+'\
+                                \n\n 想要進行匿名對話，請輸入[淦話 頻道編號 對話內容]';
+		        return rply;
+		}else{
+			rply[0] = 'push';
+		        bot.push(talkChannal[Channal-1],myText);
+		        return rply;
+		}
 	}
 }
 
