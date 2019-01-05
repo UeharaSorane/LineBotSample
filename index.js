5cdb85d179c51893e4e999259fcbea333699449a
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-05 22:00:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,21 +31,21 @@ bot.on('message', function(event) {
 		var msg = event.message;
 		
 		if(event.message.type == 'text'){
-			rply = exports.analytics.parseInput(msg.text, src.userId, profile.displayName, src.groupId).then(function(){
-				if(rply[0] == 'none'){
-					if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
-						var say = profile.displayName+'說:'+event.message.text;
-						if(event.source.groupId != null)say+= '(群組)';
-						bot.push('U7b7830437667bf4b7b54eaf02e762690',say);
-					}
-				}else if(rply[0] == 'rply'){
-					event.reply(rply[1]).then(function (data) {
-					  // success
-					}).catch(function (error) {
-					  // error
-					});
+			rply = exports.analytics.parseInput(msg.text, src.userId, profile.displayName, src.groupId);
+			
+			if(rply[0] == 'none'){
+				if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
+					var say = profile.displayName+'說:'+event.message.text;
+					if(event.source.groupId != null)say+= '(群組)';
+					bot.push('U7b7830437667bf4b7b54eaf02e762690',say);
 				}
-			});		
+			}else if(rply[0] == 'rply'){
+				event.reply(rply[1]).then(function (data) {
+				  // success
+				}).catch(function (error) {
+				  // error
+				});
+			}	
 		}
 	});
 });
