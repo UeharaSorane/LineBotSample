7b866026a627889db9fb8f2e6873a9c687c88249
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-06 01:14:00 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -43,6 +43,7 @@ function parseInput(inputStr,UserID,UserN,GroupID) {
 ////////////////////////CoC資料
 	else if (trigger.match(/^角色基本資料$/) != null) return exports.cocCha.SearchCha(UserID) ;//角色基本資料
 	else if (trigger.match(/^角色素質資料$/) != null) return exports.cocCha.ChaQuaCheck(UserID) ;//角色基本資料
+	else if (trigger.match(/^角色技能資料$/) != null) return exports.cocCha.ChaSkiCheck(UserID) ;//角色基本資料
 	
 ////////////////////////普通ROLL擲骰判定
         else if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) {
