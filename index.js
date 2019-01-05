4e67ed93135c9c5e964c1f8384533002b79790d6
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-05 21:57:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,47 +31,21 @@ bot.on('message', function(event) {
 		var msg = event.message;
 		
 		if(event.message.type == 'text'){
-			if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
-				var say = profile.displayName+'說:'+event.message.text;
-				if(event.source.groupId != null)say+= '(群組)';
-				bot.push('U7b7830437667bf4b7b54eaf02e762690',say);
-			}
-		}else if(msg.type == 'sticker'){
-			//if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
-				var say = profile.displayName+'傳送了一張貼圖';
-				if(event.source.groupId != null)say+= '(群組)';
-				event.reply(msg);
-				//bot.push('U7b7830437667bf4b7b54eaf02e762690',msg);
-				/*var Stk = {
-						type: 'sticker',
-						packageId: '2591664',
-						stickerId: '1062629'}
-				
-				bot.push('U7b7830437667bf4b7b54eaf02e762690',Stk);*/
-			//}
+			rply = exports.analytics.parseInput(msg.text, src.userId, profile.displayName, src.groupId).then(function(){
+				if(rply[0] == 'none'){
+					if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
+						var say = profile.displayName+'說:'+event.message.text;
+						if(event.source.groupId != null)say+= '(群組)';
+						bot.push('U7b7830437667bf4b7b54eaf02e762690',say);
+					}
+				}else if(rply[0] == 'rply'){
+					event.reply(rply[1]).then(function (data) {
+					  // success
+					}).catch(function (error) {
+					  // error
+					});
+				}
+			});		
 		}
 	});
-	
-	if(event.message.type == 'text'){
-		event.source.profile().then(function (profile) {
-			/*if(event.source.userId != 'U7b7830437667bf4b7b54eaf02e762690'){
-				var say = profile.displayName+'說:'+event.message.text
-				if(event.source.groupId != null)say+= '(群組)';
-				bot.push('U7b7830437667bf4b7b54eaf02e762690',say);
-			}*/
-			rply = exports.analytics.parseInput(msg.text, event.source.userId, profile.displayName, event.source.groupId);
-			if(rply[0] == 'none'){
-			}else if(rply[0] == 'groupRply'){
-				bot.push('Ca06e35d5eefc0162348764ce8bdb52b5',rply[1]);
-			}else if(rply[0] == 'rply'){
-				event.reply(rply[1]).then(function (data) {
-				  // success
-				}).catch(function (error) {
-				  // error
-			});
-			}
-
-		});
-
-	}
 });
