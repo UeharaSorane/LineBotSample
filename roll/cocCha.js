ed4ea91db6caa4d32d00d449b0b995f7117a1dc1
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-07 01:58:44 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -469,6 +469,39 @@ function ChaWeapon(UserID){
 	return rply;
 }
 
+/*function CoCmenu(UserID,UserN){
+	rply[0] = 'rply';
+	rply[1] = {
+		  "type": "template",
+		  "altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
+		  "template": {
+		  	"type": "buttons",
+		  		"text":UserN + "目前使用的角色是",
+		  		"actions": [{
+					"type": "message",
+					"label": "開啟CoC選單",
+					"text": "CoC選單"},
+					{
+					"type": "message",
+					"label": "開啟娛樂選單",
+					"text": "娛樂選單"
+					},
+		      			{
+					"type": "message",
+					"label": "打開幫助",
+					"text": "help"
+					},
+					{
+					"type": "message",
+					"label": "閒聊",
+					"text": "嘿空音"
+					}
+				]
+		}
+	}
+	return rply;
+}*/
+
 module.exports = {
 	SearchCha,
 	ChaQuaCheck,
