1a233cd6c26ed45c7cb3d42042937b643c9915b0
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-07 01:52:48 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -40,12 +40,25 @@ function parseInput(inputStr,UserID,UserN,GroupID) {
 	else if (trigger.match(/運氣|運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣
 	else if (trigger.match(/flag|插旗/) != null) return exports.funny.BStyleFlagSCRIPTS() ;//插旗用指令
 	else if (trigger.match(/^help$|^幫助$/)!= null ) return exports.Help.Help(UserN);//幫助頁面
-////////////////////////CoC資料
+	
+////////////////////////目錄
 	else if (trigger.match(/^目錄|menu$/) != null) return exports.Testing.menu(UserN) ;//目錄
+	else if (trigger.match(/^CoC選單$/) != null) return exports.Testing.CoCmenu(UserN) ;//CoC選單
+	//else if (trigger.match(/^娛樂選單$/) != null) return exports.Testing.Funnymenu(UserN) ;//娛樂選單
+	
+////////////////////////CoC選單
+	else if (trigger.match(/^CoC角色資料$/) != null) return exports.Testing.Chamenu(UserID) ;//角色資料
+	else if (trigger.match(/^技能查詢$/) != null) return exports.cocCha.ChaSkiSearch(UserID,mainMsg[1]) ;//技能查詢
+	//else if (trigger.match(/^CoC持有物資料$/) != null) return exports.Testing.Itemmenu(UserID) ;//持有物資料
+	//else if (trigger.match(/^其他CoC選項$/) != null) return exports.cocCha.OtherCoC(UserID) ;//切換角色
+	
+////////////////////////CoC角色資料
 	else if (trigger.match(/^角色基本資料$/) != null) return exports.cocCha.SearchCha(UserID) ;//角色基本資料
 	else if (trigger.match(/^角色素質資料$/) != null) return exports.cocCha.ChaQuaCheck(UserID) ;//角色素質資料
 	else if (trigger.match(/^角色技能資料$/) != null) return exports.cocCha.ChaSkiCheck(UserID) ;//角色技能資料
-	else if (trigger.match(/^技能查詢$/) != null) return exports.cocCha.ChaSkiSearch(UserID,mainMsg[1]) ;//技能查詢
+
+////////////////////////CoC持有物資料
+	else if (trigger.match(/^角色武器資料$/) != null) return exports.cocCha.ChaWeapon(UserID) ;//技能查詢
 	
 ////////////////////////普通ROLL擲骰判定
         else if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) {
