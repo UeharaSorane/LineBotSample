17138422c29ce862b92813ea58100f21a527a7e4
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-07 11:02:39 +0800

    Update Testing.js

--- roll/Testing.js
+++ roll/Testing.js
@@ -142,9 +142,33 @@ function menu(UserN){
 	return rply;
 }
 
+function funnymenu(UserN){
+	rply[0] = 'rply';
+	rply[1] = {
+		  "type": "template",
+		  "altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
+		  "template": {
+		  	"type": "buttons",
+		  		"text":"嗨！" + UserN + "，想要做什麼？",
+		  		"actions": [{
+					"type": "message",
+					"label": "今日運氣",
+					"text": "今天的運氣"},
+					{
+					"type": "message",
+					"label": "立死旗",
+					"text": "插旗"
+					}
+				]
+		}
+	}
+	return rply;
+}
+
 module.exports = {
 	ReplyTest,
 	PushTest,
 	SecretTalk,
-	menu
+	menu,
+	funnymenu
 };
