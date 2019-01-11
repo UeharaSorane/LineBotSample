5e3b706b1e66d4174ba996311c16357a2e3a7aad
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 12:21:12 +0800

    Update cocChaSheet.js

--- roll/cocChaSheet.js
+++ roll/cocChaSheet.js
@@ -21,7 +21,7 @@ DB.useServiceAccountAuth(creds, function (err) {
 						'max-col': 2,
 						'return-empty': true
 					},function(err,cells){
-						var cell = cells[0];
+						var cell = cells[1];
 						console.log('Cell R'+cell.row+'C'+cell.col+' = '+cell.value);
 					});
 					
