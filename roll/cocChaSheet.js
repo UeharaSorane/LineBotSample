a615bd025b4a3cee31d3c8d3c8f07def2a42f03f
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 12:13:00 +0800

    Update cocChaSheet.js

--- roll/cocChaSheet.js
+++ roll/cocChaSheet.js
@@ -4,17 +4,29 @@ var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('../client_secret.json');
 
 var DB = new GoogleSpreadsheet('1hwFlTrJ7JHeWMLbHmfg7LP7f13OfAoMebF6HIkHpHPs');
+var ChaAccess = [];
 var ChaIm= [];
 var ChaQua= [];
 
 DB.useServiceAccountAuth(creds, function (err) {
-	DB.getCells(8,{
-		'min-row': 1,
-		'max-row': 15,
-		'return-empty': true
-	},function(err,cells){
-		var cell = cells[1];
-		console.log('Cell R'+cell.row+'C'+cell.col+' = '+cell.value);
+	DB.getRows(8 , function (err, rows) {
+			if (err) {
+				console.log( err );
+			}else{
+				for(var i=0; i< rows.length; i++){
+					DB.getCells(rows[i].saveworksheet,{
+						'min-row': 2,
+						'max-row': 2,
+						'min-col': 2,
+						'max-col': 8,
+						'return-empty': true
+					},function(err,cells){
+						var cell = cells[1];
+						console.log('Cell R'+cell.row+'C'+cell.col+' = '+cell.value);
+					});
+					
+				}
+				console.log('角色連結資料 讀取完成');
+			}	
 	});
-	
 });
