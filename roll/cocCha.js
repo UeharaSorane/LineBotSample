e2796d0641b6c04d86acac3811b763b8c93bb678
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 18:13:49 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -2,15 +2,20 @@ var rply = [];
 
 var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('../client_secret.json');
+var DB = [];
 
-var DB = new GoogleSpreadsheet('1hwFlTrJ7JHeWMLbHmfg7LP7f13OfAoMebF6HIkHpHPs');
+
+DB[0] = new GoogleSpreadsheet('1hwFlTrJ7JHeWMLbHmfg7LP7f13OfAoMebF6HIkHpHPs');
+DB[1] = new GoogleSpreadsheet('1PUuThi4P4mTcogdN28xEmH2R1VVbbiVzTIsoKRtLNSY');
 var AccessDB= [];
+var ChaAcc = [];
+var ChaIm = [];
 
 
-DB.useServiceAccountAuth(creds, function (err) {
+DB[0].useServiceAccountAuth(creds, function (err) {
  // 先將資料讀進陣列
 	
-	DB.getRows(2 , function (err, rows) {
+	DB[0].getRows(2 , function (err, rows) {
 		if (err) {
 			console.log( err );
 		}else{
@@ -23,15 +28,47 @@ DB.useServiceAccountAuth(creds, function (err) {
 				AccessDB[i][3] = rows[i].transkey;
 				AccessDB[i][4] = Number(rows[i].transio);
 			}
-			console.log(AccessDB);
+			//console.log(AccessDB);
 			console.log('帳號連結資料 讀取完成');
 		}	
 	});
 	
-	DB.getRows(3 , function (err, rows) {
-		
+	DB[0].getRows(3 , function (err, rows) {
+		if (err) {
+			console.log( err );
+		}else{
+			for(var i=0; i< rows.length; i++){
+				ChaAcc[i] = [];
+
+				ChaAcc[i][0] = rows[i].chaname;
+				ChaAcc[i][1] = Number(rows[i].savesheet);
+				ChaAcc[i][2] = Number(rows[i].saveworksheet);
+				
+				DB[ChaAcc[i][1]].useServiceAccountAuth(creds, function (err) {
+					DB[ChaAcc[i][1]].getCells(ChaAcc[i][2],{
+						'min-row' : 2,
+						'max-row' : 8,
+						'min-col' : 2,
+						'max-col' : 2,
+						'return-empty' : true
+						
+					},function(err,cells){
+						if(err) return err;
+						else{
+							console.log(cells);
+						}
+					});
+				});
+				
+			}
+			console.log(ChaAcc);
+			console.log('角色連結資料 讀取完成');
+		}
+	
 	});
 });
+
+
 
 function CreateAccount(UserID){
 	rply[0] = 'rply';
@@ -187,8 +224,8 @@ function receiveAccount(UserID,playCha,TransKey){
 }
 
 function SaveAccessDB(Target){
-	DB.useServiceAccountAuth(creds, function (err) {
-		DB.getRows(2 , function (err, rows) {
+	DB[0].useServiceAccountAuth(creds, function (err) {
+		DB[0].getRows(2 , function (err, rows) {
 			if (err) {
 				console.log( err );
 			}else{
