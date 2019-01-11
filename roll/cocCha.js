83e64c233bb391e13429860f0096524d02c0275f
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 14:42:32 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -46,22 +46,20 @@ function CreateAccount(UserID,PlayerN){
 		AccessDB[i] = [UserID,'','','none',0];
 		
 		DB.useServiceAccountAuth(creds, function (err) {
-			if(err) console.log(err);
-			else{
-				DB.addRow(1,{
-					userid: UserID,
-					playcha: '',
-					havecha: '',
-					transkey: "none",
-					transio: 0
-				},function(err,row){
-					if(err){
-						console.log(err);
-						rply[1] = '錯誤!資料寫入失敗';
-						return rply;
-					}
-				});
-			}
+			//if(err) console.log(err);
+			DB.addRow(1,{
+				userid: UserID,
+				playcha: '',
+				havecha: '',
+				transkey: "none",
+				transio: 0
+			},function(err,row){
+				if(err){
+					console.log(err);
+					rply[1] = '錯誤!資料寫入失敗';
+					return rply;
+				}
+			});
 			
 		});
 
