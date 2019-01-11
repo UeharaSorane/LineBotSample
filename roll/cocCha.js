e2e3c6fbe80e85d908da369a8d3b18c6aec1dc29
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 23:59:03 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -1,16 +1,4 @@
 var rply = [];
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://b88009005:b09050905@ds229312.mlab.com:29312/linetest';
-
-//連線到資料庫
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-
-db.on('error',console.error.bind(console, 'MongoDB connection error:'));
-
-
-
 
 module.exports = {
 
