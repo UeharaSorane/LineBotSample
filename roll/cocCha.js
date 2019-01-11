d6f2dd0135228cd58d552eae18170e75f5aeb1aa
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-11 19:17:58 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -34,7 +34,7 @@ DB[0].useServiceAccountAuth(creds, function (err) {
 });
 
 for(var a = 1;a<DB.length;a++){
-	console.log(a);
+	console.log(DB(a));
 	//if(DB[a])
 	
 	//DB[a].useServiceAccountAuth(creds, function (err) {
