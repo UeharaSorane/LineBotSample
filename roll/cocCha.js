6af317d368fee39cc15a3f071b227b08cc73e9fb
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-12 23:33:36 +0800

    Update cocCha.js

--- roll/cocCha.js
+++ roll/cocCha.js
@@ -1,5 +1,20 @@
 var rply = [];
 
+//////////mongo系統
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://b88009005:b09050905@ds229312.mlab.com:29312/linetest';
+
+//連線測試
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+mongoose.connect(mongoDB, function (err) {
+	if(err) throw err;
+	else console.log('連線成功，可以正常吃芒果!');
+});
+///
+
 module.exports = {
 
 };
