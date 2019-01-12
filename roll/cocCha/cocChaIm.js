2f406a4be35f175ca6bdba6d213b45ca3d0d6104
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-13 02:56:02 +0800

    Update cocChaIm.js

--- roll/cocCha/cocChaIm.js
+++ roll/cocCha/cocChaIm.js
@@ -11,12 +11,13 @@ const Schema = mongoose.Schema;
 
 var ChaImSchema = new Schema({
 	id:{type:Number},
+	user_id:{type:String,require:true},
 	cha_name:{type:String,require:true},
-	cha_class:{type:String,require:true},
-	age:{type:String,require:true},
-	sex:{type:String,require:true},
-	born:{type:String,require:true},
-	live:{type:String,require:true}
+	cha_class:{type:String,default'未設定'},
+	age:{type:String,default'未設定'},
+	sex:{type:String,default'未設定'},
+	born:{type:String,default'未設定'},
+	live:{type:String,default'未設定'}
 });
 
 var ChaIm = mongoose.model('ChaIm',ChaImSchema);
@@ -31,6 +32,22 @@ ChaIm.find(function(err,ChaIms){
 	console.log("coc角色基本資料，讀取完成");
 });
 
+function CreateCha(UserID,chaName){
+	var CIAL = CIArr.length;
+	CIArr[CIAL] = {
+		id:CIAL,
+		user_id:UserID,
+		cha_name:chaName,
+		cha_class:'未設定',
+		age:'未設定',
+		sex:'未設定',
+		born:'未設定',
+		live:'未設定'
+	}
+	
+	saveChaIm(CIArr[CIAL]);
+}
+
 function findChaIm(chaName){
 	for(var a =0;a<CIArr.length;a++){
 		if(CIArr[a].cha_name == chaName) return CIArr[a];
@@ -38,6 +55,18 @@ function findChaIm(chaName){
 	return 'NotFound';
 }
 
+function saveChaIm(ChaImT){
+	CIArr.findOneAndUpdate({id: ChaImT.id},ChaImT,{
+		upsert:true
+	},function(err){
+		if(err) throw err;
+		else{
+			console.log("角色基本資料 更新完成");
+		}
+	});
+}
+
 module.exports = {
-	findChaIm
+	findChaIm,
+	CreateCha
 };
