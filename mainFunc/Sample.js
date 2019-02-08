1c582b8af069c6e1c386fc623900375bc284f06c
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-02-08 20:20:17 +0800

    Update Sample.js

--- mainFunc/Sample.js
+++ mainFunc/Sample.js
@@ -1,12 +1,3 @@
-function Sample(SendText) {
-	//本函數為教學用函數，可以直接將使用者傳來的訊息顯示在後台且原封不動地丟給使用者
-	//SendText 為使用本函數必須提供的資料
-	console.log(SendText);//讓後臺顯示使用者的訊息
-	
-	return SendText;//把訊息原封不動丟回分析區，讓他丟回去
-
-}
-
 function Hello(){
 	
 	var rply = '你好!';//定義機器人要說啥
@@ -16,6 +7,5 @@ function Hello(){
 }
 
 module.exports = {
-	Sample,
 	Hello
 };//這個部分是為了讓使用這個.js的程式知道哪些函數能用
