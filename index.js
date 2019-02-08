bedb2eee6368a671be2c9c3b23874a426d1e2d32
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-02-08 20:42:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //引用外部函數庫
 var linebot = require('linebot');//LineBot 的主要功能
+var express = require('express');//express 用來架網站以和bot溝通
 //引用自己的函數庫
 var analytics = require("./modules/analytics.js");//這個是分析訊息的函庫
 //
@@ -8,6 +9,20 @@ var bot = linebot({
   	channelSecret: process.env.LINE_CHANNEL_SECRET, //這裡是讓系統抓在Heroku設定的數據
   	channelAccessToken: process.env.LINE_CHANNEL_ACCESSTOKEN // 同上
 });
+
+///以下是用來控制網頁與Bot的關係
+var app = express();
+app.post('/', bot.parser());
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+	var port = server.address().port;
+	console.log("App now running on port", port);
+	console.log("...好，系統正常啟動");
+});
+app.get('/', function (req, res) {
+	res.send('<h1>恭喜，程式沒有錯誤!記得複製這個網頁的網址然後貼到「Webhook URL」的地方喔!</h1>');
+});
+///
 
 var rply = '';//建立一個裝要回覆的訊息的變數
 
